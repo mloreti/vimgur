10aeb9f300fda3b956bcdfc77511267972895fc3
Mickey Loreti <mloreti10@gmail.com>
2016-11-04 16:19:59 -0700

    Remove slider

---
+++ frontend/util/video_slider.js
@@ -0,0 +1,29 @@
+$(function() {
+    var getImages = () => {
+      var images = $(".video-square").find("img");
+      var imgs = Object.keys(images).map( id => (images[id].currentSrc )).splice(0,8));
+      return imgs;
+    }
+
+    var setImage = image => {
+      $(".videos-hero").css("background-image", `url(${image})`);
+    }
+
+    var idx = 0;
+
+    var imageSlider = () => {
+      var images = getImages();
+      if (idx == 7){
+        idx = 0;
+      } else {
+        idx++;
+      }
+      setImage(images[idx]);
+    }
+
+    setTimeout( () => {
+      var images = getImages();
+      setImage(images[0]);
+    }, 100)
+    setInterval(imageSlider, 3000);
+});
