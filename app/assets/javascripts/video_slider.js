80e2e7d2033777d25fcf61d45b2a9be6edd29477
Mickey Loreti <mloreti10@gmail.com>
2016-11-04 16:11:17 -0700

    convert slider to es5

--- app/assets/javascripts/video_slider.js
+++ app/assets/javascripts/video_slider.js
@@ -1,17 +1,17 @@
 $(function() {
-    var getImages = () => {
+    var getImages = function() {
       var images = $(".video-square").find("img");
       var imgs = Object.keys(images).map(id => (images[id].currentSrc )).splice(0,8);
       return imgs;
     }
 
-    var setImage = image => {
+    var setImage = function(image) {
       $(".videos-hero").css("background-image", `url(${image})`);
     }
 
     var idx = 0;
 
-    var imageSlider = () => {
+    var imageSlider = function() {
       var images = getImages();
       if (idx == 7){
         idx = 0;
@@ -20,7 +20,7 @@ $(function() {
       }
       setImage(images[idx]);
     }
-    setTimeout(()=>{
+    setTimeout(function(){
       var images = getImages();
       setImage(images[0]);
     }, 100)
