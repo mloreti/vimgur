39b68e10eb8778d3134f3da6207f4361b0bb3991
Mickey Loreti <mloreti10@gmail.com>
2016-11-07 21:00:53 -0800

    Add comment validation

--- frontend/components/comments/comments.jsx
+++ frontend/components/comments/comments.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router';
 import Ago from 'react-ago-component';
+import Modal from 'react-modal';
+
+const customStyles = {
+  overlay : {
+      backgroundColor   : 'rgba(0, 0, 0, 0.75)'
+  }
+};
 
 class Comments extends React.Component {
 
@@ -8,9 +15,24 @@ class Comments extends React.Component {
     super(props);
     this.state = {
       body: "",
-      video_id: this.props.video.id
+      video_id: this.props.video.id,
+      open: false
     };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal () {
+    this.setState({open: true});
+  }
+
+  closeModal () {
+    this.setState({open: false});
+  }
+
+  componentWillMount() {
+    Modal.setAppElement('body');
   }
 
   update(field) {
@@ -21,11 +43,16 @@ class Comments extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    let comment = {};
-    comment.video_id = this.props.video.id;
-    comment.body = this.state.body;
-    this.props.addComment(comment);
-    this.setState({body: ""});
+    console.log(this.props.session);
+    if (this.props.session.currentUser){
+      let comment = {};
+      comment.video_id = this.props.video.id;
+      comment.body = this.state.body;
+      this.props.addComment(comment);
+      this.setState({body: ""});
+    } else {
+      this.openModal();
+    }
   }
 
   renderComments() {
@@ -65,6 +92,17 @@ class Comments extends React.Component {
           {this.renderComments()}
           {this.commentsForm()}
         </div>
+        <Modal
+          isOpen={this.state.open}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          className="like-modal"
+          >
+          <h5>You must be signed in to comment</h5>
+          <Link to="/login">Login </Link>
+           or
+          <Link to="/signup"> Sign Up</Link>
+        </Modal>
       </div>
     )
   }
--- frontend/components/comments/comments_container.js
+++ frontend/components/comments/comments_container.js
@@ -2,9 +2,12 @@ import { connect } from 'react-redux';
 import Comments from './comments';
 import { addComment } from '../../actions/comment_actions';
 
-const mapStateToProps = ({ videos, video }) => {
-  return {videos: videos,
-  video: video}
+const mapStateToProps = ({ videos, video, session }) => {
+  return {
+    videos: videos,
+    video: video,
+    session: session
+  }
 };
 
 const mapDispatchToProps = dispatch => ({
