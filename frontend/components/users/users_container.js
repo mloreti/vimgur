d651bd8166300bbb628c365220ed08c42a0f5a32
Mickey Loreti <mloreti10@gmail.com>
2016-11-12 19:06:13 -0800

    Fix for heroku login issue

--- frontend/components/users/users_container.js
+++ frontend/components/users/users_container.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import Users from './users';
 
 const mapStateToProps = ({ session }) => ({
-  user: session.currentUser
+  user: session.currentUser ? session.currentUser : null
 });
 
 const mapDispatchToProps = dispatch => ({
